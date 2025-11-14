da916a029ce719150a833727691f1a9d79710453
Yug Dandawala <yugdandawala3@gmail.com>
2025-11-14 10:12:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,74 @@
+from fastapi import FastAPI, Request, Form, HTTPException,File,UploadFile
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from database import Emp, database
+from fastapi.staticfiles import StaticFiles
+import shutil
+import os
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+app = FastAPI(title="Employee CRUD")
+templates = Jinja2Templates(directory="templates")
+
+# Connect and disconnect database automatically
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    query = Emp.select()
+    employees = await database.fetch_all(query)
+    return templates.TemplateResponse("index.html", {"request": request, "employees": employees})
+
+
+@app.post("/add/", response_class=RedirectResponse)
+async def add_employee(id: int = Form(...), username: str = Form(...), email: str = Form(...), pno: str = Form(...),image:UploadFile=File(...)):
+   file_path = os.path.join(UPLOAD_DIR, f"Employee{id}.jpg")
+   with open(file_path,"wb") as buffer:
+      shutil.copyfileobj(image.file, buffer)
+   
+   query = Emp.insert().values(Id=id, Username=username, Email=email, P_no=pno,Image=file_path)
+   await database.execute(query)
+   return RedirectResponse("/",status_code=302)
+
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+
+@app.get("/delete/{emp_id}", response_class=RedirectResponse)
+async def delete_employee(emp_id: int):
+   query = Emp.delete().where(Emp.c.Id == emp_id)
+   await database.execute(query)
+   return RedirectResponse(url="/",status_code=302)
+
+# Update form for particular Id
+@app.get("/edit/{emp_id}", response_class=HTMLResponse)
+async def edit_form(request: Request, emp_id: int):
+   query = Emp.select().where(Emp.c.Id == emp_id)
+   emp = await database.fetch_one(query)
+   if not emp:
+      raise HTTPException(status_code=404, detail="Employee not found")
+   return templates.TemplateResponse("update.html", {"request": request, "emp": emp})
+
+@app.post("/update/{emp_id}", response_class=RedirectResponse)
+async def update_employee(emp_id: int, username: str = Form(...), email: str = Form(...), pno: str = Form(...),image:UploadFile=File(...)):
+   file_path = os.path.join(UPLOAD_DIR, f"Employee{id}.jpg")
+   with open(file_path,"wb") as buffer:
+      shutil.copyfileobj(image.file, buffer)
+
+   query = Emp.update().where(Emp.c.Id == emp_id).values(Username=username, Email=email, P_no=pno,Image=file_path)
+   await database.execute(query)
+   return RedirectResponse(url="/",status_code=302)
+
+
 # from fastapi import FastAPI, Request, File, UploadFile
 # from fastapi.responses import HTMLResponse
 # from fastapi.templating import Jinja2Templates
@@ -164,74 +235,3 @@
 #             return book
 #     # If not found, raise 404
 #     raise HTTPException(status_code=404, detail="Book not found")
-
-
-from fastapi import FastAPI, Request, Form, HTTPException,File,UploadFile
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
-from database import Emp, database
-from fastapi.staticfiles import StaticFiles
-import shutil
-import os
-
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
-
-app = FastAPI(title="Employee CRUD")
-templates = Jinja2Templates(directory="templates")
-
-# Connect and disconnect database automatically
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    query = Emp.select()
-    employees = await database.fetch_all(query)
-    return templates.TemplateResponse("index.html", {"request": request, "employees": employees})
-
-
-@app.post("/add/", response_class=RedirectResponse)
-async def add_employee(id: int = Form(...), username: str = Form(...), email: str = Form(...), pno: str = Form(...),image:UploadFile=File(...)):
-   file_path = os.path.join(UPLOAD_DIR, f"Employee{id}.jpg")
-   with open(file_path,"wb") as buffer:
-      shutil.copyfileobj(image.file, buffer)
-   
-   query = Emp.insert().values(Id=id, Username=username, Email=email, P_no=pno,Image=file_path)
-   await database.execute(query)
-   return RedirectResponse("/",status_code=302)
-
-
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
-
-@app.get("/delete/{emp_id}", response_class=RedirectResponse)
-async def delete_employee(emp_id: int):
-   query = Emp.delete().where(Emp.c.Id == emp_id)
-   await database.execute(query)
-   return RedirectResponse(url="/",status_code=302)
-
-# Update form for particular Id
-@app.get("/edit/{emp_id}", response_class=HTMLResponse)
-async def edit_form(request: Request, emp_id: int):
-   query = Emp.select().where(Emp.c.Id == emp_id)
-   emp = await database.fetch_one(query)
-   if not emp:
-      raise HTTPException(status_code=404, detail="Employee not found")
-   return templates.TemplateResponse("update.html", {"request": request, "emp": emp})
-
-@app.post("/update/{emp_id}", response_class=RedirectResponse)
-async def update_employee(emp_id: int, username: str = Form(...), email: str = Form(...), pno: str = Form(...),image:UploadFile=File(...)):
-   file_path = os.path.join(UPLOAD_DIR, f"Employee{id}.jpg")
-   with open(file_path,"wb") as buffer:
-      shutil.copyfileobj(image.file, buffer)
-
-   query = Emp.update().where(Emp.c.Id == emp_id).values(Username=username, Email=email, P_no=pno,Image=file_path)
-   await database.execute(query)
-   return RedirectResponse(url="/",status_code=302)
